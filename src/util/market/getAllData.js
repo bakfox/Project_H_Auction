a96336ddfa0bb8748affa776cff16d974b620029
bakfox <153157605+bakfox@users.noreply.github.com>
2025-04-30 14:47:58 +0900

    feat : 오타 수정
    입니다.

--- src/util/market/getAllData.js
+++ src/util/market/getAllData.js
@@ -1,9 +1,9 @@
-import marketData from '../../class/marketData.class.js';
+import MarketData from '../../class/MarketData.class.js';
 import { getAllMarketData } from '../../db/marketplace/market.db.js';
 
 export async function initMarketSesion() {
   const marketAllData = await getAllMarketData();
   for (let data of marketAllData) {
-    new marketData(data);
+    new MarketData(data);
   }
 }
