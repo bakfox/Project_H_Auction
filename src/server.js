86e21a793a8c438e2066dc0b5005e4bdfda94d51
bakfox <153157605+bakfox@users.noreply.github.com>
2025-05-06 13:33:38 +0900

    fix : 오류 수정
    테스트 추가

--- src/server.js
+++ src/server.js
@@ -19,12 +19,13 @@ client.on('error', (err) => console.log('Redis Error:', err));
 
 await client.connect();
 console.log(`레디스 연결 : ${config.redis.host + config.redis.port}`);
-listenForMessages(client).catch(console.error); // 연결되었을 때 대기 시작
+listenForMessages(client).catch(console.log); // 연결되었을 때 대기 시작
 
 // 초기화 한번
 await client.flushDb();
 initMarketSesion();
 
+await client.rPush('BUY', "hello");
 await client.rPush('BUY', "hello");
 
 // 대기 처리
